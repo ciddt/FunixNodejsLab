6dfc78c785c99841689bed417464840fb74e0c4e
MarcellNT <100115775+MarcellNT@users.noreply.github.com>
2022-07-31 09:21:49 +0700

    Sử dụng Express Router

--- app.js
+++ app.js
@@ -5,20 +5,13 @@ const express = require('express');
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
+const adminRouters = require('./routes/admin');
+const shopRouters = require('./routes/shop');
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
-})
+app.use(adminRouters);
+app.use(shopRouters);
 
 const server = http.createServer(app);
 
