7a4962a94ce195ced90d06b9d7473408de2ab250
MarcellNT <100115775+MarcellNT@users.noreply.github.com>
2022-07-31 09:48:24 +0700

    Giới hạn việc thực thi Middleware đối với Post Requests

--- app.js
+++ app.js
@@ -18,8 +18,6 @@ app.post('/product', (req, res, next) => {
 
 app.use('/', (req, res, next) => {
     res.send('<h1>Hello from Express!</h1>');
-})
+});
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
