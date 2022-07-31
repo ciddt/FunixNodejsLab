4ccf8d6c1ff61473ce4cccbe4cf99837ed1c5311
MarcellNT <100115775+MarcellNT@users.noreply.github.com>
2022-07-31 08:51:22 +0700

    Phân tích cú pháp Requests đến

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const http = require('http');
+const bodyParser = require('body-parser');
 
 const express = require('express');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('This always run!');
-    next();
-});
+app.use(bodyParser.urlencoded());
 
 app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware')
-    res.send('<h1>The "Add Product" Page</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
 app.use('/', (req, res, next) => {
-    console.log("In another middleware");
-    res.send('<h1>Hello from Express!</h1>')
+    res.send('<h1>Hello from Express!</h1>');
 })
 
 const server = http.createServer(app);
