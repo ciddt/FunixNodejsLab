fe7400765a9522585ba313d249c286e5e0b76354
MarcellNT <100115775+MarcellNT@users.noreply.github.com>
2022-07-31 09:44:10 +0700

    Xử lý các Route khác nhau

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ app.use('/add-product', (req, res, next) => {
 app.use('/', (req, res, next) => {
     console.log("In another middleware");
     res.send('<h1>Hello from Express!</h1>')
-})
+});
 
 app.listen(3000);
