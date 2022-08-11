3499278b0a301b63f41503dc2a6f3801cdebadc4
MarcellNT <100115775+MarcellNT@users.noreply.github.com>
2022-08-11 18:39:37 +0700

    Tạo và quản lý người dùng tạm thời

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 
+const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -13,9 +15,21 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { use } = require('./routes/admin');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -25,7 +39,19 @@ app.use(errorController.get404);
 sequelize
   .sync()
   .then(result => {
-    // console.log(result);
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if(!user) {
+      return User.create({
+        name: 'Max',
+        email: 'test@test.com'
+      })
+    } else {
+      return user
+    }
+  })
+  .then(user => {
     app.listen(3000);
   })
   .catch(err => {
