0e471d9e92336c2e4ef822ecca44ffd2274e1f5d
MarcellNT <100115775+MarcellNT@users.noreply.github.com>
2022-09-06 09:22:04 +0700

    Tổng kết bài học kết nối cơ sở dữ liệu

--- util/database.js
+++ util/database.js
@@ -1,16 +1,30 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://linhthuynguyen2589:239923ngu@funixnjs101xcluster.wbywvok.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://linhthuynguyen2589:239923ngu@funixnjs101xcluster.wbywvok.mongodb.net/shop?retryWrites=true&w=majority'
   )
     .then(client => {
       console.log('Connected!');
-      callback(client);
+      _db = client.db();
+      callback();
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
 }
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!'
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
 
