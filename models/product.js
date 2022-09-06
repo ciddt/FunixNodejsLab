a907e483735470bc9a13fab7920735862514994a
MarcellNT <100115775+MarcellNT@users.noreply.github.com>
2022-09-06 10:06:45 +0700

    Tìm nạp tất cả các sản phẩm

--- models/product.js
+++ models/product.js
@@ -10,7 +10,7 @@ class Product {
 
   save() {
     const db = getDb();
-    return db.collection("products")
+    return db.collection('products')
       .insertOne(this)
       .then((result) => {
         console.log(result);
@@ -18,6 +18,21 @@ class Product {
       .catch((err) => {
         console.log(err);
       });
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection('products')
+      .find()
+      .toArray()
+      .then(products => {
+        console.log(products);
+        return products;
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
 }
 
