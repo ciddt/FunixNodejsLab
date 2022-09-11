1ead6591213ddab7c4545dbd04300b0636b5271c
MarcellNT <100115775+MarcellNT@users.noreply.github.com>
2022-09-11 20:02:22 +0700

    Thiết lập Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,13 @@
 exports.getLogin = (req, res, next) => {
-    res.render('auth/login', {
-      path: '/login',
-      pageTitle: 'Login',
-      isAuthenticated: req.isLoggedIn
-    });
-  };
-  
-  exports.postLogin = (req, res, next) => {
-      req.isLoggedIn = true;
-      res.redirect('/');
-    };
-  
+  const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[0];
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: isLoggedIn,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  res.setHeader("Set-Cookie", "loggedIn=true");
+  res.redirect("/");
+};
