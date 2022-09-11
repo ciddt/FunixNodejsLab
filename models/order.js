1e019e09f0ace27c24db391700f23b0e61905bdb
MarcellNT <100115775+MarcellNT@users.noreply.github.com>
2022-09-11 17:25:41 +0700

    Tạo & Nhận đơn hàng

---
+++ models/order.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+  products: [
+    {
+      product: { type: Object, require: true },
+      quantity: { type: Number, require: true },
+    },
+  ],
+  user: {
+    name: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+  },
+});
+
+module.exports = mongoose.model('Order', orderSchema);
