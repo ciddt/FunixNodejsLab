e65491edeec15bf099d38aba43462c0d2bff4f85
MarcellNT <100115775+MarcellNT@users.noreply.github.com>
2022-09-11 20:05:42 +0700

    Thao tác Cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[0];
+  const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[0] === 'true';
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
